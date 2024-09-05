6aebe7b74952fc4e118edb4d9aec54332b100bff
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2024-09-04 22:53:19 -0300

    Create index.js

---
+++ JS/Ejercicio extra/index.js
@@ -0,0 +1,9 @@
+let nombreUsuario = prompt("Ingrese su nombre de usuario", "");
+document.write("Hola " + nombreUsuario, "</br>");
+
+let num1 = Number(prompt("Ingrese el 1er npumero", ""));
+let num2 = Number(prompt("Ingrese el 2do numero", ""));
+document.write("El resultado de la suma es: ", num1 + num2, "</br>");
+document.write("El resultado de la resta es: ", num1 - num2, "</br>");
+document.write("El resultado de la multiplicacion es: ", num1 * num2, "</br>");
+document.write("El resultado de la division es: ", num1 / num2, "</br>");
