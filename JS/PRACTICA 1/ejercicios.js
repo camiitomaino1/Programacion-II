bb7e565d3495f07dc2e61675b309a46dbb22823b
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2024-09-04 22:00:49 -0300

    Update ejercicios.js

--- JS/PRACTICA 1/ejercicios.js
+++ JS/PRACTICA 1/ejercicios.js
@@ -1,3 +1,13 @@
-
+//ejercicio 1
 alert("Hello world");
-document.write("Hello World");
+//ejercicio 2
+document.write("Hello World</br>");
+//ejercicio 3
+document.write("El resltado de la suma es: ", 3 + 5);
+//ejercicio 4
+let nombreUsuario = prompt("Ingrese su nombre de usuario", "");
+document.write("Hola " + nombreUsuario);
+//ejercicio 5
+let num1 = Number(prompt("Ingrese el 1er npumero", ""));
+let num2 = Number(prompt("Ingrese el 2do numero", ""));
+document.write("El resultado de la suma es: ", num1 + num2);
