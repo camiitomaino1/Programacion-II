75335a5cb4d0037310bd01e7c7f936d1c30dd78c
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2024-09-09 18:54:11 -0300

    Update ejercicios.js

--- JS/PRACTICA 1/ejercicios.js
+++ JS/PRACTICA 1/ejercicios.js
@@ -1,5 +1,5 @@
 //ejercicio 1
-alert("Hello world");
+/*alert("Hello world");
 //ejercicio 2
 document.write("Hello World</br>");
 //ejercicio 3
@@ -11,3 +11,13 @@ document.write("Hola " + nombreUsuario);
 let num1 = Number(prompt("Ingrese el 1er npumero", ""));
 let num2 = Number(prompt("Ingrese el 2do numero", ""));
 document.write("El resultado de la suma es: ", num1 + num2);
+
+//Ejercicio 6
+var n1 = Number("Ingrese el primer numero");
+var n2 = Number("Ingrese el segundo numero");*/
+
+if(n1 > n2) {
+    alert("El numero mas grande es " + n1);
+} else {
+    alert("El numero mas grande es " + n2);
+}
