c325e903f652714bb73143ef8dafcdfb159b1d40
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2024-09-09 20:56:56 -0300

    Create index.js

---
+++ JS/PRACTICA 2/index.js
@@ -0,0 +1,8 @@
+//Ejercicio 1
+function repetirMensaje(numero) {
+    for(let i = 0; i < numero; i++){
+        console.log("Bienvenidos al curso Full Stack" + "</br>");
+    }
+}
+
+repetirMensaje(3);
