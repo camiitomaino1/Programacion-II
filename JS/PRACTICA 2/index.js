c87246fa093171a7220ef8400c1aeb31c971c454
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2024-10-08 14:59:49 -0300

    ejercici 3

--- JS/PRACTICA 2/index.js
+++ JS/PRACTICA 2/index.js
@@ -1,7 +1,20 @@
-//Ejercicio 1
+/*//Ejercicio 1
 function repetirMensaje(numero) {
     for(let i = 0; i < numero; i++){
         console.log("Bienvenidos al curso Full Stack\n");
     }
 }
+
+//Ejercicio 2
+function maximo(num1, num2){
+    if(num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+document.write(maximo(3, 4))*/
+
+//Ejercicio 3
+
 
